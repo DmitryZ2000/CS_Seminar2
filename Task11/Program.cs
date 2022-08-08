1cc03e8169f1d29011e32c9ae0e1fb3b02308654
DVZ <polo1215@bk.ru>
2022-08-08 20:56:50 +0300

    Seminar-2 Задачки

---
+++ Task11/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(10, 1000); //[10,99]
+Console.WriteLine(number);
+int digit1 = number / 100;
+int digit2 = number % 10;
+int newdigit = digit1 * 10 + digit2;
+Console.WriteLine(newdigit);
---
+++ Task9/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(10,100); //[10,99]
+int digit1 = number / 10;
+int digit2 = number % 10;
+Console.WriteLine(number);
+if (digit1 > digit2)
+    Console.WriteLine(digit1);
+else
+    Console.WriteLine(digit2);
