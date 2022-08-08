8719de385203cf3b26334b71f69dd385791080b7
DVZ <polo1215@bk.ru>
2022-08-08 23:42:46 +0300

    добавил ввод с кклавиатуры и конвертацию string в int

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,9 +1,11 @@
-﻿int number1 = new Random().Next(1, 1000); //[1,1000]
-int number1 = 161;
-Console.WriteLine(number1);
-if ((number1 % 7 == 0) && (number1 % 23 == 0))
+﻿// int number1 = new Random().Next(1, 1000); //[1,1000]
+// int number1 = 161;
+Console.Write("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number);
+if ((number % 7 == 0) && (number % 23 == 0))
 {
-    Console.WriteLine("Кратно");
+    Console.WriteLine("Кратно 7 и 23 одновременн");
 }
 else
-    Console.WriteLine("Не кратно");
+    Console.WriteLine("Не кратно 7 и 23 одновременн");
--- Task16/Program.cs
+++ Task16/Program.cs
@@ -1,8 +1,16 @@
 ﻿// int number1 = new Random().Next(1, 1000); //[1,1000]
 // int number2 = new Random().Next(1, 1000); //[1,1000]
-int number1 = 25;
-int number2 = 51;
+// int number1 = 25;
+// int number2 = 51;
+Console.Write("Введите любое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите любое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
 if (number1*number1 == number2 || number2*number2 == number1)
-    Console.WriteLine("Да");
+{
+    Console.WriteLine("Одно число является квадратом другого числа");
+}
+    
 else
-    Console.WriteLine("Нет");
+    Console.WriteLine("Числа не являются квадратами друг друга");
