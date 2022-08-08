fbca32752058819d68a4ba68a13591876e2c470a
DVZ <polo1215@bk.ru>
2022-08-08 21:44:41 +0300

    Seminar-2 Задача-16

---
+++ Task14/Program.cs
@@ -0,0 +1,9 @@
+﻿int number1 = new Random().Next(1, 1000); //[1,1000]
+int number1 = 161;
+Console.WriteLine(number1);
+if ((number1 % 7 == 0) && (number1 % 23 == 0))
+{
+    Console.WriteLine("Кратно");
+}
+else
+    Console.WriteLine("Не кратно");
---
+++ Task16/Program.cs
@@ -0,0 +1,8 @@
+﻿// int number1 = new Random().Next(1, 1000); //[1,1000]
+// int number2 = new Random().Next(1, 1000); //[1,1000]
+int number1 = 25;
+int number2 = 51;
+if (number1*number1 == number2 || number2*number2 == number1)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
