2412ffd2c61270bf65822b80e72d600db7d858e9
DVZ <polo1215@bk.ru>
2022-08-08 21:11:14 +0300

    Добавил вывод остатка

---
+++ Task12/Program.cs
@@ -0,0 +1,12 @@
+﻿int number1 = new Random().Next(10, 100); //[10,99]
+int number2 = new Random().Next(1, 10); //[10,99]
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+if ((number1 % number2) == 0)
+    Console.WriteLine("Кратно");
+else
+{
+    Console.Write("Не кратно. Остаток от деления:  ");
+    Console.WriteLine(number1 % number2);
+}
+    
